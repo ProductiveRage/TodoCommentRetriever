8f68a0f1b2365b3e2b0e895d6a5728b878e7b4ed
dan <dan@newmind.co.uk>
2014-10-30 22:47:56 +0000

    Initial commit (to accompany a Blog Post I'm writing)

---
+++ TODOCommentMapper/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Evaluation;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TODOCommentMapper
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var solutionFilePath = @"..\..\..\TODOCommentMapper.sln";
+
+			var todoCommentIdentifier = new ToDoCommentIdentifier();
+			foreach (var csharpCompileFile in GetProjectFilesForSolution(new FileInfo(solutionFilePath)).SelectMany(projectFile => GetCSharpCompileItemFilesForProject(projectFile)))
+			{
+				foreach (var todoComment in todoCommentIdentifier.GetToDoComments(csharpCompileFile.OpenText().ReadToEnd()))
+				{
+					Console.WriteLine(todoComment.Content);
+					Console.WriteLine();
+					if (todoComment.NamespaceIfAny == null)
+						Console.WriteLine("Not in any namespace");
+					else
+					{
+						Console.WriteLine("Namespace: " + todoComment.NamespaceIfAny.Name);
+						if (todoComment.TypeIfAny != null)
+							Console.WriteLine("Type: " + todoComment.TypeIfAny.Identifier);
+						if (todoComment.MethodOrPropertyIfAny != null)
+						{
+							Console.Write("Method/Property: ");
+							if (todoComment.MethodOrPropertyIfAny is ConstructorDeclarationSyntax)
+								Console.Write(".ctor");
+							else if (todoComment.MethodOrPropertyIfAny is MethodDeclarationSyntax)
+								Console.Write(((MethodDeclarationSyntax)todoComment.MethodOrPropertyIfAny).Identifier);
+							else if (todoComment.MethodOrPropertyIfAny is PropertyDeclarationSyntax)
+								Console.Write(((PropertyDeclarationSyntax)todoComment.MethodOrPropertyIfAny).Identifier);
+							else
+								Console.Write("?");
+							Console.WriteLine();
+						}
+					}
+					Console.WriteLine(csharpCompileFile.FullName + ":" + todoComment.LineNumber);
+					Console.WriteLine();
+				}
+			}
+
+			Console.WriteLine("Success! Press [Enter] to continue..");
+			Console.ReadLine();
+		}
+
+		private static IEnumerable<FileInfo> GetProjectFilesForSolution(FileInfo solutionFile)
+		{
+			if (solutionFile == null)
+				throw new ArgumentNullException("solutionFile");
+
+			var projectFileMatcher = new Regex(
+				@"Project\(""\{\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\}""\) = ""(.*?)"", ""(?<projectFile>(.*?\.csproj))"", ""\{\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\}"""
+			);
+			foreach (Match match in projectFileMatcher.Matches(solutionFile.OpenText().ReadToEnd()))
+				yield return new FileInfo(Path.Combine(solutionFile.Directory.FullName, match.Groups["projectFile"].Value));
+		}
+
+		private static IEnumerable<FileInfo> GetCSharpCompileItemFilesForProject(FileInfo projectFile)
+		{
+			if (projectFile == null)
+				throw new ArgumentNullException("projectFile");
+
+			return (new ProjectCollection()).LoadProject(projectFile.FullName).AllEvaluatedItems
+				.Where(item => item.ItemType == "Compile")
+				.Select(item => item.EvaluatedInclude)
+				.Where(include => include.EndsWith(".cs", StringComparison.OrdinalIgnoreCase))
+				.Select(include => new FileInfo(Path.Combine(projectFile.Directory.FullName, include)));
+		}
+	}
+}
---
+++ TODOCommentMapper/ToDoCommentIdentifier.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TODOCommentMapper
+{
+	public class ToDoCommentIdentifier
+	{
+		private readonly Predicate<string> _toDoCommentMatcher;
+		public ToDoCommentIdentifier(Predicate<string> toDoCommentMatcher)
+		{
+			if (toDoCommentMatcher == null)
+				throw new ArgumentNullException("toDoCommentMatcher");
+
+			_toDoCommentMatcher = toDoCommentMatcher;
+		}
+		public ToDoCommentIdentifier() : this(DefaultToDoCommentFilter) { }
+
+		public static Predicate<string> DefaultToDoCommentFilter
+		{
+			get
+			{
+				// TODO: Tidy this up with a reg ex?
+				return commentContent =>
+					commentContent.Contains("//TODO") ||
+					commentContent.Contains("// TODO") ||
+					commentContent.Contains("TODO:") ||
+					commentContent.Contains("TODO[") ||
+					commentContent.Contains("TODO [") ||
+					commentContent.Contains("TODO\r") ||
+					commentContent.Contains("TODO\n") ||
+					commentContent.EndsWith("TODO");
+			}
+		}
+
+		public IEnumerable<Comment> GetToDoComments(string content)
+		{
+			if (content == null)
+				throw new ArgumentException("content");
+
+			var todoComments = new List<Comment>();
+			var commentLocatingVisitor = new CommentLocatingVisitor(
+				comment =>
+				{
+					if (_toDoCommentMatcher(comment.Content))
+						todoComments.Add(comment);
+				}
+			);
+			commentLocatingVisitor.Visit(
+				CSharpSyntaxTree.ParseText(content).GetRoot()
+			);
+			return todoComments;
+		}
+
+		private class CommentLocatingVisitor : SyntaxWalker
+		{
+			private readonly Action<Comment> _commentLocated;
+			public CommentLocatingVisitor(Action<Comment> commentLocated) : base(SyntaxWalkerDepth.StructuredTrivia)
+			{
+				if (commentLocated == null)
+					throw new ArgumentNullException("commentLocated");
+
+				_commentLocated = commentLocated;
+			}
+
+			private static HashSet<SyntaxKind> _commentTypes = new HashSet<SyntaxKind>(new[] {
+				SyntaxKind.SingleLineCommentTrivia,
+				SyntaxKind.MultiLineCommentTrivia,
+				SyntaxKind.DocumentationCommentExteriorTrivia,
+				SyntaxKind.SingleLineDocumentationCommentTrivia,
+				SyntaxKind.MultiLineDocumentationCommentTrivia
+			});
+
+			public override void Visit(SyntaxNode node)
+			{
+				var nodeType = node.GetType();
+				base.Visit(node);
+			}
+
+			protected override void VisitToken(SyntaxToken token)
+			{
+				var tokenType = token.GetType();
+				base.VisitToken(token);
+			}
+
+			protected override void VisitTrivia(SyntaxTrivia trivia)
+			{
+				if (_commentTypes.Contains(trivia.CSharpKind()))
+				{
+					string triviaContent;
+					using (var writer = new StringWriter())
+					{
+						trivia.WriteTo(writer);
+						triviaContent = writer.ToString();
+					}
+
+					// Note: When looking for the containingMethodOrPropertyIfAny, we want MemberDeclarationSyntax types such as ConstructorDeclarationSyntax, MethodDeclarationSyntax,
+					// IndexerDeclarationSyntax, PropertyDeclarationSyntax but NamespaceDeclarationSyntax and TypeDeclarationSyntax also inherit from MemberDeclarationSyntax and we
+					// don't want those
+					var containingNode = trivia.Token.Parent;
+					var containingMethodOrPropertyIfAny = TryToGetContainingNode<MemberDeclarationSyntax>(
+						containingNode,
+						n => !(n is NamespaceDeclarationSyntax) && !(n is TypeDeclarationSyntax)
+					);
+					var containingTypeIfAny = TryToGetContainingNode<TypeDeclarationSyntax>(containingNode);
+					var containingNameSpaceIfAny = TryToGetContainingNode<NamespaceDeclarationSyntax>(containingNode);
+					_commentLocated(new Comment(
+						triviaContent,
+						trivia.SyntaxTree.GetLineSpan(trivia.Span).StartLinePosition.Line,
+						containingMethodOrPropertyIfAny,
+						containingTypeIfAny,
+						containingNameSpaceIfAny
+					));
+				}
+				base.VisitTrivia(trivia);
+			}
+
+			private T TryToGetContainingNode<T>(SyntaxNode node, Predicate<T> optionalFilter = null) where T : SyntaxNode
+			{
+				if (node == null)
+					throw new ArgumentNullException("node");
+
+				var currentNode = node;
+				while (true)
+				{
+					var nodeOfType = currentNode as T;
+					if (nodeOfType != null)
+					{
+						if ((optionalFilter == null) || optionalFilter(nodeOfType))
+							return nodeOfType;
+					}
+					if (currentNode.Parent == null)
+						break;
+					currentNode = currentNode.Parent;
+				}
+				return null;
+			}
+		}
+	}
+}
